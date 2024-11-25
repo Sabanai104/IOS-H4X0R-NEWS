d0fb2ef03ef47d1e25cbeaa4ee47ae30e1e5af41
Gabriel Sabanai <gabrielsabanaitrindade@gmail.com>
2024-11-25 01:15:04 -0300

    Initial Commit

---
+++ H4X0RTests/H4X0RTests.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RTests.swift
+//  H4X0RTests
+//
+//  Created by Gabriel Sabanai on 25/11/24.
+//
+
+import Testing
+@testable import H4X0R
+
+struct H4X0RTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
